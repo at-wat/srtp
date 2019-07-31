34348eb8724a6aacb56e27fc27d21a296f9031be
Atsushi Watanabe <atsushi.w@ieee.org>
2019-07-31 14:38:52 +0900

    Update test

--- session_srtcp_test.go
+++ session_srtcp_test.go
@@ -4,33 +4,38 @@ import (
 	"net"
 	"runtime"
 	"testing"
+	"time"
 )
 
 func TestSessionSRTCPLeak(t *testing.T) {
 	_, cli := net.Pipe()
-	finalized := false
-	func() {
-		sess, err := NewSessionSRTCP(cli, &Config{
-			Keys: SessionKeys{
-				make([]byte, 16),
-				make([]byte, 14),
-				make([]byte, 16),
-				make([]byte, 14),
-			},
-			Profile: ProtectionProfileAes128CmHmacSha1_80,
-		})
-		if err != nil {
-			t.Fatalf("Failed to create SessionSRTCP: %v", err)
+
+	sess, err := NewSessionSRTCP(cli, &Config{
+		Keys: SessionKeys{
+			make([]byte, 16),
+			make([]byte, 14),
+			make([]byte, 16),
+			make([]byte, 14),
+		},
+		Profile: ProtectionProfileAes128CmHmacSha1_80,
+	})
+	if err != nil {
+		t.Fatalf("Failed to create SessionSRTCP: %v", err)
+	}
+
+	finalized := make(chan struct{})
+	runtime.SetFinalizer(sess, func(interface{}) {
+		close(finalized)
+	})
+	defer func() {
+		// sess is Closed and unreferenced. It must be finalized by GC.
+		runtime.GC()
+		select {
+		case <-finalized:
+		case <-time.After(time.Second):
+			t.Error("SessionSRTCP is not finalized.")
 		}
-		runtime.SetFinalizer(sess, func(*SessionSRTCP) {
-			finalized = true
-		})
-		sess.Close()
 	}()
 
-	// sess is now Closed and unreferenced; It must be finalized by GC.
-	runtime.GC()
-	if !finalized {
-		t.Error("SessionSRTCP is expected to be finalized after unreferenced, but not be finalized.")
-	}
+	sess.Close()
 }
--- session_srtp_test.go
+++ session_srtp_test.go
@@ -4,33 +4,38 @@ import (
 	"net"
 	"runtime"
 	"testing"
+	"time"
 )
 
 func TestSessionSRTPLeak(t *testing.T) {
 	_, cli := net.Pipe()
-	finalized := false
-	func() {
-		sess, err := NewSessionSRTP(cli, &Config{
-			Keys: SessionKeys{
-				make([]byte, 16),
-				make([]byte, 14),
-				make([]byte, 16),
-				make([]byte, 14),
-			},
-			Profile: ProtectionProfileAes128CmHmacSha1_80,
-		})
-		if err != nil {
-			t.Fatalf("Failed to create SessionSRTP: %v", err)
+
+	sess, err := NewSessionSRTP(cli, &Config{
+		Keys: SessionKeys{
+			make([]byte, 16),
+			make([]byte, 14),
+			make([]byte, 16),
+			make([]byte, 14),
+		},
+		Profile: ProtectionProfileAes128CmHmacSha1_80,
+	})
+	if err != nil {
+		t.Fatalf("Failed to create SessionSRTP: %v", err)
+	}
+
+	finalized := make(chan struct{})
+	runtime.SetFinalizer(sess, func(interface{}) {
+		close(finalized)
+	})
+	defer func() {
+		// sess is Closed and unreferenced. It must be finalized by GC.
+		runtime.GC()
+		select {
+		case <-finalized:
+		case <-time.After(time.Second):
+			t.Error("SessionSRTP is not finalized.")
 		}
-		runtime.SetFinalizer(sess, func(*SessionSRTP) {
-			finalized = true
-		})
-		sess.Close()
 	}()
 
-	// sess is now Closed and unreferenced; It must be finalized by GC.
-	runtime.GC()
-	if !finalized {
-		t.Error("SessionSRTP is expected to be finalized after unreferenced, but not be finalized.")
-	}
+	sess.Close()
 }
